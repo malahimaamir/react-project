706050c06f60ffd1a10fc243ed750076f1bc871b
malahimaamir <malahimaamir@gmail.com>
2024-11-16 08:12:05 +0500

    app completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,45 +1,65 @@
 import React, {useState} from 'react'
 
+
 export default function TextForm(props) {
-    const [text, settext] = useState('');
-    const textchangetoupppercase = () => {    
-        let newtext = text.toUpperCase();
-        settext(newtext);
-    }
-    const textchangetolowercase = () => {    
-        let newtext = text.toLowerCase();
-        settext(newtext);
-    }
-    
-    const textclear = () => {    
-        let newtext = ('');
-        settext(newtext);
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
     }
 
-    
-     const handleonchange = (event) => {    
-    settext(event.target.value);
- }
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
 
-  return (
-    <>
-    <div className='container'>    
-        <h1>{props.heading}</h1> 
-<div className="mb-3"> 
-  <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="8"></textarea> 
-</div>
-<button type="button" class="btn btn-danger mx-1" onClick={textchangetoupppercase}>Convert to uppercase</button>
-<button type="button" class="btn btn-danger mx-1" onClick={textchangetolowercase}>Convert to Lowercase</button>
-<button type="button" class="btn btn-danger mx-1" onClick={textclear}>CLear text</button>
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
 
-    </div>
-    <div className='container my-3'>
-        <h2>Your text summary:</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008*text.split(" ").length}Minutes read</p>
-        <h3>Preview:</h3>
-        <p>{text}</p>
-    </div>
-    </>
-  )
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+
+    // Credits: A
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    // Credits: Coding Wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+        </>
+    )
 }
